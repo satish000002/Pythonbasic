9b777a10522c0126f1fade4940c3bff15a38d5c8
satish000002 <satish000002@gmail.com>
2023-11-21 01:46:01 +0530

    first day kalyani tho sradha ............

---
+++ Hospatal Data_With Bullion.py
@@ -0,0 +1,3 @@
+full_name = 'Y V Satish'
+age = 20
+is_new = True
---
+++ Index_of_Carector.py
@@ -0,0 +1,6 @@
+course1 = 'Python course for Beginners'
+       # [0123456]
+print(course1[0])
+print(course1[-1])
+print(course1[0:-1])
+print(course1[4:-1])

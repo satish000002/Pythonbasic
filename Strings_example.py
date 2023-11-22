50d0b53669eba3ab49e00aeb71278e088362ba33
satish000002 <satish000002@gmail.com>
2023-11-22 16:46:02 +0530

    Day one notes

--- Strings_example.py
+++ Strings_example.py
@@ -1,2 +1,9 @@
 course = 'Python for beiggners'
 print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.find('Y'))
+print(course.replace('Python','mec python'))
+print('Python' in course)
+print('yvs' in course)

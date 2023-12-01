542cbea0682aac3a99436563c007acc3fe42825d
satish000002 <satish000002@gmail.com>
2023-12-01 10:22:01 +0530

    Operatot Precedence exampale - 1

--- Operator precedence.py
+++ Operator precedence.py
@@ -1,3 +1,14 @@
 x = 10 + 3 * 2
 print(x)
-#  1st *   
+#  1st *
+y = 10 + 3 * 2 **2
+print(y)
+#BRACKETS 1ST
+#Expotantion **  2ND
+#or division   3RD
+#Multiplication  4TH
+#Addition or    5TH
+#Subtractio    6TH
+
+
+

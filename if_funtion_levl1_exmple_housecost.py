3076cf714a094a0dcee3eed904cba0966bf7154c
satish000002 <satish000002@gmail.com>
2023-12-10 10:45:46 +0530

    if _ funtion example wit the simple house Down payment caliculation

---
+++ if_funtion_levl1_exmple_housecost.py
@@ -0,0 +1,17 @@
+price= 10000000
+buyer_good_credit = True
+
+if buyer_good_credit:
+    down_payment = price * 0.1
+    print(down_payment)
+    print(f"Byer have to pay ten percent  ${down_payment}")
+    print("they need to put Dowen 10%")
+
+
+else:
+    down_payment = price * 0.2
+    print(down_payment)
+    print(f"Byer have to pay Twenty percent  ${down_payment}")
+    print("they need to put down 20%")
+
+print("enjoy the Day")

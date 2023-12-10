ec174a1c6580ffe72f0adfa8db8b1324eb2b8cd7
satish000002 <satish000002@gmail.com>
2023-12-10 09:55:09 +0530

    adding funtions if basic file examle

---
+++ if_ functions.py
@@ -0,0 +1,17 @@
+#if it a hot
+ #   its a hot day
+  #   Drink pleanty of water
+#otherwiser if its cold
+#    it is a cold day
+ #   ware warm clothes
+# otherwise
+
+#is_hot = True
+is_hot = False
+
+if is_hot:
+    print("it is a hot day")
+    print("drink Plenty of water")       #shift + tab
+print("enjoy the day")
+#--------------------------------------------
+
---
+++ modules.py
@@ -0,0 +1,14 @@
+import math
+x = 2.9
+#math.ceil(x)   __  Celing level number
+
+print(math.ceil(x))
+
+print(math.floor(x))
+
+#math.floor    __ floor level number
+
+#python 3 math module ---- do gogole it
+
+
+

f68cf784368f5e4a9ce91dfc09ed00d09efa1c52
satish000002 <satish000002@gmail.com>
2023-12-10 10:19:03 +0530

    if funtion example with using if and elif   also using the shift + tab to change the line of fintion

---
+++ if_funtion_Level1_Basic.py
@@ -0,0 +1,15 @@
+#is_hot = True
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("it is a hot day")
+    print("drink Plenty of water")    #shift + tab
+elif is_cold:
+    print("it is a cold day")
+    print("ware the warm cloathes ")
+
+else:
+    print("its lovely day ")
+
+print("enjoy the day")                #shift + tab

dce4013f7275968a62904a4b91e0506493a292a1
satish000002 <satish000002@gmail.com>
2023-12-10 10:08:04 +0530

    basic level of the if function useing added

---
+++ if_condition_Lev1.py
@@ -0,0 +1,10 @@
+#is_hot = True
+is_hot = False
+
+if is_hot:
+    print("it is a hot day")
+    print("drink Plenty of water")    #shift + tab
+else:
+    print("it is a cold day")
+    print("ware the warm cloathes ")
+    print("enjoy the day")
